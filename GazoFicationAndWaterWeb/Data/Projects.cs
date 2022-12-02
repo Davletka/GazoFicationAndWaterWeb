1b5e498da954c9d58a897e0b3e45cd1870686b09
Булат Давлетшин <kizaqa123@gmail.com>
2022-12-02 15:43:23 +0300

    cd

--- GazoFicationAndWaterWeb/Data/Projects.cs
+++ GazoFicationAndWaterWeb/Data/Projects.cs
@@ -10,10 +10,14 @@ namespace GazoFicationAndWaterWeb.Data
         [BsonIgnoreIfDefault]
         public string? Name { get; set; }
         [BsonIgnoreIfDefault]
+        public string? CustomerName { get; set; }
+        [BsonIgnoreIfDefault]
         public string? Theme { get; set; }
         [BsonIgnoreIfDefault]
         public Developer? developer { get; set; }
         [BsonIgnoreIfDefault]
         public Designer? designer { get; set; }
+
+        public List<Documents> Documents { get; set; }
     }
 }
