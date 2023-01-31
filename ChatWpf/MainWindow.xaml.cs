4ff9ae79e56015c7d6bd3fc47d911efc9f9c4af0
Булат Давлетшин <kizaqa123@gmail.com>
2023-01-31 21:32:24 +0300

    d

---
+++ ChatWpf/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7168/chat")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.On<string, string>("Receive", (user, message) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    chatbox.Items.Insert(0, newMessage);
+                });
+            });
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // подключемся к хабу
+                await connection.StartAsync();
+                chatbox.Items.Add("Вы вошли в чат");
+                sendBtn.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                chatbox.Items.Add(ex.Message);
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // отправка сообщения
+                await connection.InvokeAsync("Send", userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                chatbox.Items.Add(ex.Message);
+            }
+        }
+
+        private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            await connection.InvokeAsync("Send", "", $"Пользователь {userTextBox.Text} выходит из чата");
+            await connection.StopAsync();   // отключение от хаба
+        }
+    }
+}
